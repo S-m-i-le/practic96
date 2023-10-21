a926b1a5305c74833fd542cef5deec0b5837001c
yuriykvyatkovskyy <yuriykreik20021997@gmail.com>
2023-10-21 13:03:18 +0300

    task-2

---
+++ js/task2.js
@@ -0,0 +1,14 @@
+//  *? Напишіть програму, яка отримає від користувача
+//   *? число (кількість хвилин) та виведе в консоль
+//   *? рядок у форматі годин та хвилин
+//  *?      https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+//  *?      70 === 01:10
+
+function programShowTime(initialMinute) {
+    const hour = Math.floor(initialMinute / 60)
+    const modificateHour = String(hour).padStart(2, 0)
+    const minutes = initialMinute % 60
+    const modificateMinutes = String(minutes).padStart(2, 0)
+    return `${modificateHour}:${modificateMinutes}`
+}
+console.log(programShowTime(600))
